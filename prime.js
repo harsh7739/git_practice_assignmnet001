a4a94431a0af8fe576a0648285f5973fc3a0ea4c
harsh7739 <hrbhumi123@gmail.com>
2022-11-06 21:55:09 +0530

    created prime file

---
+++ prime.js
@@ -0,0 +1,11 @@
+   let num = 13;
+   let count =0;
+     for(let i=num;i<=0;i++){
+        count++;
+      }
+        if(count==2){
+          console.log("No. is prime");
+         }
+        else {
+         console.log("No. is not Prime");
+        }
